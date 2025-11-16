356b14aaa2c704fac76fa642b6c4232ca96bfc6a
Glenn254 <168417561+Glenn254@users.noreply.github.com>
2025-11-16 17:39:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const current = 678;
+    const target = 1000;
+    const remaining = target - current;
 
+    document.getElementById("current").textContent = current;
+    document.getElementById("remaining").textContent = remaining;
+
+    const progress = Math.round((current / target) * 100);
+    document.getElementById("progress-percentage").textContent = progress + "%";
+    document.getElementById("progress").style.width = progress + "%";
+});
